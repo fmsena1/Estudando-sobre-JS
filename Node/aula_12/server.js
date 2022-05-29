f6a2dce402abb59695d8d082c83d5e44451a664a
Filipe Magalhães Sena <filipemagalhaessena@gmail.com>
2022-05-29 15:24:47 -0300

    Aprendendo sobre webpack +express

---
+++ Node/aula_12/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes'); 
+const path = require('path');
+
+app.use(express.urlencoded({extended: true}));
+//Conteúdo estático
+app.use(express.static(path.resolve(__dirname, 'public')));
+//Renderização absoluta
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+//Instalando EJS = npm i ejs
+app.set('view engine', 'ejs');
+app.use(routes);
+app.listen(3000, ()=> {
+    console.log('http://localhost:3000');
+    console.log('Servidor exucutando na porta 3000');
+});
