bcc5b2fc3397f6aff8340fe43321be448ba8e26f
Filipe Magalhães Sena <filipemagalhaessena@gmail.com>
2022-05-30 13:43:04 -0300

    Aprendendo sobre Mongo
    Instalado o dotenv, e mongoose

--- Node/aula_14/src/controllers/homeController.js
+++ Node/aula_14/src/controllers/homeController.js
@@ -1,3 +1,16 @@
+const HomeModel = require('../models/HomeModels');
+
+HomeModel.create({
+    titulo: 'Era uma vez',
+    decricao: 'Livro Infântil'
+},
+    {
+    titulo: 'teste',
+    decricao: 'teste123'
+    }
+)
+    .then( dados => console.log(dados))
+    .catch(err => console.log(err));
 exports.homePage = (req, res) =>{
     //res.render = renderizar page 
     res.render('index');
